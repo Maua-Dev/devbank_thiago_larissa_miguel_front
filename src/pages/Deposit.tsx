c4138dc4dad2b9d825425e5c16f0a7eba091d7d4
Miguel Reny <259689109+MiguellReny@users.noreply.github.com>
2026-04-23 20:05:21 -0300

    feat: estrutura inicial com dependências corrigidas

---
+++ src/pages/Deposit.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Deposit() {
+  return <h1>Deposit</h1>
+}
+
+export default Deposit
---
+++ src/pages/History.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function History() {
+  return <h1>History</h1>
+}
+
+export default History
